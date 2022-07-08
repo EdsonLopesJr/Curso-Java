0250bf93001444c0fbf0cbfeb1350b1549850b86
Edson Lopes Jr <edson.eldsjr@gmail.com>
2022-07-08 07:17:42 -0300

    Commit
    Compreendendo a questão das Heranças
    
    Comparando os encapsulamento e os comportamentos do modificadores de acessos e os  Getters e Setters.

--- exercicios/src/oo/heranca/desafio/Ferrari.java
+++ exercicios/src/oo/heranca/desafio/Ferrari.java
@@ -2,16 +2,16 @@ package oo.heranca.desafio;
 
 public class Ferrari extends Carro {
 	
-	Ferrari() {
+	public Ferrari() {
 		this(410);
 	}
 	
-	Ferrari(int velocidadeMaxima){
+	public Ferrari(int velocidadeMaxima){
 		super(velocidadeMaxima);
 		delta = 15;
 	}
 //	@Override
-//	void acelerar() {
+//	public void acelerar() {
 //		velocidadeAtual += 15;
 //	}
 }
